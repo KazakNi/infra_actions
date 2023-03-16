9a0122ea33aaab709ed1171f9156a31049d8586b
Nikita Kazakov <kazakov.ni@yandex.ru>
2023-03-16 14:54:39 +0300

    fix:passphrase correction

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('У меня получилось! <br> URRRAAAAAAAAAA')
+    return HttpResponse('У меня получилось! <br> URRRAAAAAA')
 
 
 def second_page(request):
